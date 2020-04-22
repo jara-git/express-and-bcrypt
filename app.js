def209d140b5654c15ce16050202aeea593920df
solar <jarabelmonte@gmail.com>
2020-04-22 14:58:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+
+app.listen(3000, (error) => {
+    if (error){
+        console.log(chalk.red.inverse('error executing app.listen'))
+    } else {
+        console.log ('connected at port 3000')
+    }
+})
+
